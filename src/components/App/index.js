1625209c642810aca90bd1b680e6842511b51718
Andrew Ballantyne <ballantyno@gmail.com>
2017-06-17 19:33:54 -0400

    Drops navigation header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,12 @@
 import React, { PureComponent } from 'react';
 
-import { Navigation } from '/components';
+// import { Navigation } from '/components';
 import { Content } from '/containers';
 
 class App extends PureComponent {
   render() {
     return (
       <div>
-        <Navigation />
         <Content />
       </div>
     );
